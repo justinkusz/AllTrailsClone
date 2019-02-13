c27799bd317c63919822bcc4676aa85f92e98fef
Justin Kusz <justin.kusz@gmail.com>
2019-02-12 22:50:31 -0500

    hike card redesign

--- components/common/HikeCard.js
+++ components/common/HikeCard.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Text, ImageBackground } from "react-native";
 
 import StarRating from "./StarRating";
 
-class HikeCard extends React.PureComponent {
+class HikeCard extends React.Component {
   render() {
-    const { id, snapshotURL, title, rating, date } = this.props.track;
+    const { snapshotURL, title, rating } = this.props.track;
+    const date = this.props.track.stats.date;
 
     return (
-      <Card
-        key={id}
-        image={{ uri: snapshotURL }}
-        imageProps={{ resizeMode: "cover" }}
+      <ImageBackground
+        source={{ uri: snapshotURL }}
+        style={{ width: "100%", height: 150 }}
+        resizeMode="cover"
       >
-        <Text style={{ fontWeight: "bold", fontSize: 18 }}>{title}</Text>
+        <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+          {title}
+        </Text>
         <StarRating rating={rating} />
-        <Text>{date}</Text>
-      </Card>
+        <Text style={{ color: "white", fontSize: 12 }}>{date}</Text>
+      </ImageBackground>
     );
   }
 }
